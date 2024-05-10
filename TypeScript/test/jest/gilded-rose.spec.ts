474fc6bff3470447fc4c97560cb4409247b45e15
Samuel Owen <SamHirael@gmail.com>
2024-05-10 15:20:22 +0100

    chore: Refactor degradeItem function to accept an object with item and amount properties

--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -9,6 +9,6 @@ describe('Gilded Rose', () => {
 
   it("can degrade an item", () => {
     const itemToDegrade = new Item("degradable", 1, 50)
-    expect(degradeItem(itemToDegrade, 10)).toEqual(new Item("degradable", 1, 40))
+    expect(degradeItem({ item: itemToDegrade, amount: 10 })).toEqual(new Item("degradable", 1, 40))
   });
 });
